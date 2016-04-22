7880d34fb7a7bfabd65043f9d7ddd0ab26c39a91
Kevin Hamer <kevin@imarc.com>
2016-04-22 14:08:05 -0400

    giving it some functionality

--- padstone/Install.php
+++ padstone/Install.php
@@ -5,12 +5,31 @@ class Install
 {
     static public function run()
     {
-        $install = new Install(getcwd());
+        $install = new Install();
     }
 
-    public function __construct($root)
+    public function __construct()
     {
-        echo "Installing to $root...\n";
-        echo "done";
+        $zip_file = 'craft_' . date('c') . '.zip';
+
+        echo "Downloading Craft from https://craftcms.com/latest.zip...\n";
+
+        file_put_contents(
+            $zip_file,
+            fopen("https://craftcms.com/latest.zip?accept_license=yes", "r")
+        );
+
+        echo "Created ZIP file '$zip_file'. Extracting contents...\n";
+
+        $zip = new ZipArchive();
+        if ($zip->open($zip_file) === true) {
+            $zip->extractTo('.');
+            $zip->close();
+        } else {
+            echo "'$zipfile' is not a valid ZIP archive.\n";
+            exit(1);
+        }
+
+        echo "Extraction complete.\n";
     }
 }
