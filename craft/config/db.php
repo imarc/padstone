bbf801debc8c46c1071b21a600e4e97a040ccc13
Kevin Hamer <kevin@imarc.com>
2016-04-25 09:43:28 -0400

    Providing starting general.php and db.php configurations

--- craft/config/db.php
+++ craft/config/db.php
@@ -5,9 +5,35 @@
  *
  * This is an example configuration file provided by Padstone that demonstrates
  * per environment configuration.
- * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-return array(
-    'foo' => 'bar'
-);
+$env = preg_replace('#^.*/#', '', dirname(CRAFT_BASE_PATH));
+
+$config = [
+    'server'      => 'localhost',
+    'user'        => 'web',
+    'tablePrefix' => '',
+];
+
+switch ($env) {
+	case 'dev':
+		$config = array_merge($config, [
+            'database' => 'dev_example_com',
+		]);
+		break;
+	case 'stage':
+		$config = array_merge($config, [
+            'database' => 'stage_example_com',
+		]);
+		break;
+	case 'prod':
+		$config = array_merge($config, [
+            'database' => 'example_com',
+		]);
+		break;
+	default:
+		die("Unfortunately, the server is misconfigured. Please review the configuration in config/general.php.");
+}
+
+return $config;
