18799023873cccf747e71b08eab1083e139cbe3a
Jeff Turcotte <jeff.turcotte@gmail.com>
2016-05-08 20:57:02 -0400

    Added dev db/general config, and .editorconfig

--- craft/config/db.php
+++ craft/config/db.php
@@ -8,36 +8,12 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-$env = preg_replace('#^.*/#', '', dirname(realpath(CRAFT_BASE_PATH)));
-
-if (strpos(CRAFT_BASE_PATH, '/vagrant/') === 0) {
-    $env = 'dev';
-}
-
 $config = [
-    'server' => 'localhost',
-    'user' => 'web',
-    'tablePrefix' => '',
+	 'server'      => 'db',
+	 'user'        => 'craft',
+	 'database'    => 'craft',
+	 'tablePrefix' => '',
+	 'password'    => 'craft',
 ];
-
-switch ($env) {
-    case 'dev':
-        $config = array_merge($config, [
-            'database' => 'dev_example_com',
-        ]);
-        break;
-    case 'stage':
-        $config = array_merge($config, [
-            'database' => 'stage_example_com',
-        ]);
-        break;
-    case 'prod':
-        $config = array_merge($config, [
-            'database' => 'example_com',
-        ]);
-        break;
-    default:
-        die("Unfortunately, the server is misconfigured. Please review the configuration in config/db.php.");
-}
 
 return $config;
