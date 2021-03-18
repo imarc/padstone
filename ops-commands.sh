678899098b4ec0aa2bfc17920ae7f1cdc51d7672
Kevin Hamer <kevin@imarc.com>
2021-03-18 17:29:59 -0400

    update ops-commands.sh for a better Padstone/ops experience

--- ops-commands.sh
+++ ops-commands.sh
@@ -3,25 +3,68 @@ ops-craft() {
     ops shell ./craft "$@"
 }
 
+ops-configure() {
+    cmd-doc "Interactively configure ops settings in your .env file."
+
+    BASENAME=$(basename $0)
+
+    DEFAULT=${BASENAME//[^a-zA-Z0-9-]/_}
+    DEFAULT=${DB_DATABASE:-$DEFAULT}
+    read -p "Database Name [$DEFAULT]: " INPUT
+    ops env DB_DATABASE ${INPUT:-$DEFAULT}
+
+    read -p "Remote host [$OPS_PROJECT_REMOTE_HOST]: " INPUT
+    ops env OPS_PROJECT_REMOTE_HOST ${INPUT:-$OPS_PROJECT_REMOTE_HOST}
+
+    read -p "Remote user [$OPS_PROJECT_REMOTE_USER]: " INPUT
+    ops env OPS_PROJECT_REMOTE_USER ${INPUT:-$OPS_PROJECT_REMOTE_USER}
+
+    read -p "Remote path [$OPS_PROJECT_REMOTE_PATH]: " INPUT
+    ops env OPS_PROJECT_REMOTE_PATH ${INPUT:-$OPS_PROJECT_REMOTE_PATH}
+
+    read -p "Remote database name [$OPS_PROJECT_REMOTE_DB_NAME]: " INPUT
+    ops env OPS_PROJECT_REMOTE_DB_NAME ${INPUT:-$OPS_PROJECT_REMOTE_DB_NAME}
+
+    read -p "Sync dirs [$OPS_PROJECT_SYNC_DIRS]: " INPUT
+    ops env OPS_PROJECT_SYNC_DIRS ${INPUT:-$OPS_PROJECT_SYNC_DIRS}
+}
+
 ops-padstone-install() {
     cmd-doc "Run this only once per project to complete the Padstone install."
 
-    ops composer install
-    ops npm install
-    ops npm run dev
+    npx imarc/boilerplate-components
+    echo -e "\n@import \"~bootstrap/scss/bootstrap\";" >> resources/styles/main.scss
+    echo -e "\nimport \"bootstrap\"" >> resources/js/main.js
+
+    ops install
+}
+
+ops-install() {
+    cmd-doc "Run this after cloning the project to setup the project for local development on Ops."
 
     if [[ ! -e .env ]]; then
         cp .env.example .env
     fi
 
-    ops craft setup/security-key
+    if [[ -z "$SECURITY_KEY" ]]; then
+        ops craft setup/security-key
+    fi
 
+    ops configure
 
-    # "npx imarc/boilerplate-components",
-    # "echo \"\n@import \\\"~bootstrap/scss/bootstrap\\\";\" >> resources/styles/main.scss",
-    # "echo \"\nimport \\\"bootstrap\\\";\" >> resources/js/main.js"
-}
+    if [[ -n "$DB_DATABASE" ]] && [[ -n "$OPS_PROJECT_REMOTE_DB_NAME" ]]; then
+        read -p "Run ops sync now [Yn]: " INPUT
+        if [[ $INPUT == 'Y' ]]; then
+            ops sync
+        fi
 
-ops-install() {
-    cmd-doc "Run this after cloning the project to setup the project for local development on Ops."
+    elif [[ -e padstone.sql ]] && [[ -n "$DB_DATABASE" ]]; then
+        ops mariadb import $DB_DATABASE < padstone.sql
+    fi
+
+    ops composer install
+    ops npm install
+    ops npm run dev
+
+    echo -n "\nVisit your site at https://$(basename $0).imarc.io/"
 }
