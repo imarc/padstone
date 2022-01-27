bc34af646c77cca39f7eeb6cdc4daed1404c1078
Kevin Hamer <kevin@imarc.com>
2022-01-27 14:55:46 -0500

    add to docblock in exampleunittest

--- tests/unit/ExampleUnitTest.php
+++ tests/unit/ExampleUnitTest.php
@@ -6,6 +6,10 @@ use UnitTester;
 use Craft;
 
 /**
+ * Example Unit Test. As a guideline, think of Unit tests as testing a specific
+ * PHP function, method, class, or setting. If you are trying to test a module,
+ * then you're in the right place.
+ *
  * Not that this file ends with Test and is not using the Codeception Cest
  * format. The Cest format is designed for functional/acceptance tests, whereas
  * using the plain Test format makes more sense for unit tests.
