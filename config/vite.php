5ba2a038170151d1f9de31a79d68569ff461b506
Kevin Hamer <kevin@imarc.com>
2022-06-13 14:54:24 -0400

    add vite.php

---
+++ config/vite.php
@@ -0,0 +1,18 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+    'useDevServer' => App::env('USE_VITE_DEV_SERVER') ?? false,
+    'manifestPath' => '@webroot/dist/manifest.json',
+    'devServerPublic' => 'http://localhost:3000/',
+    'serverPublic' => App::env('DEFAULT_SITE_URL') . '/dist/',
+    'errorEntry' => '',
+    'cacheKeySuffix' => '',
+    'devServerInternal' => '',
+    'checkDevServer' => false,
+    'includeReactRefreshShim' => false,
+    'includeModulePreloadShim' => true,
+    'criticalPath' => '@webroot/dist/criticalcss',
+    'criticalSuffix' =>'_critical.min.css',
+];
