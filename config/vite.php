be32f4d1915d3e6d185af673e8fe6e8d2c957bc2
Kevin Hamer <kevin@imarc.com>
2022-09-12 14:43:10 -0400

    update vite port

--- config/vite.php
+++ config/vite.php
@@ -5,7 +5,7 @@ use craft\helpers\App;
 return [
     'useDevServer' => App::env('USE_VITE_DEV_SERVER') ?? false,
     'manifestPath' => '@webroot/dist/manifest.json',
-    'devServerPublic' => 'http://localhost:3000/',
+    'devServerPublic' => 'http://localhost:5173/',
     'serverPublic' => App::env('DEFAULT_SITE_URL') . '/dist/',
     'errorEntry' => '',
     'cacheKeySuffix' => '',
