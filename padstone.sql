4e59c421ab9f67761cf4d0f2f0181d9ecbb22fbf
Linnea <linneahartsuyker@imarc.com>
2025-05-07 16:16:56 -0400

    Fixing database dump

--- padstone.sql
+++ padstone.sql
@@ -1,16 +1,22 @@
+--
+-- PostgreSQL database dump
+--
 
-Usage: ops mariadb [4mcommand(B[m
-Aliases: my
+-- Dumped from database version 16.3 (Debian 16.3-1.pgdg120+1)
+-- Dumped by pg_dump version 16.3 (Debian 16.3-1.pgdg120+1)
 
-Available commands:
-  [1m[32mcli (sh)(B[m   Run mariadb interactive shell.
-  [1m[32mcreate(B[m     Create a MariaDB database.
-  [1m[32mdrop(B[m       Drop a MariaDB database.
-  [1m[32mexport(B[m     Export a mariadb database via mariadb-dump.
-  [1m[32mhelp(B[m       Show this help.
-  [1m[32mimport(B[m     Import a MariaDB database.
-  [1m[32mlist (ls)(B[m  List all mariadb databases.
-  [1m[32mrun(B[m        Run SQL via mariadb.
-  [1m[32mwww(B[m        Open MariaDB in Adminer.
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
+--
+-- PostgreSQL database dump complete
+--
 
