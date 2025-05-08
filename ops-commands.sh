68e9ab468354dfebf1d05f056a35a5244acf0c53
Jeff Turcotte <jeff.turcotte@gmail.com>
2025-05-08 12:22:19 -0400

    Added correct npx command for pronto. Fixed npm scripts.

--- ops-commands.sh
+++ ops-commands.sh
@@ -34,6 +34,9 @@ ops-install() {
     echo "Calling $(tput smul)npm install$(tput rmul)..."
     npm install
 
+    echo "Calling $(tput smul)npx @imarc/pronto@latest$(tput rmul)..."
+    npx @imarc/pronto@latest --non-interactive y ./resources y y ./web
+
     # get updated settings; unusual syntax for bash 3.2.57
     source /dev/stdin <<<"$(ops env)"
 
@@ -48,7 +51,7 @@ ops-install() {
     fi
 
     echo "Calling $(tput smul)npm run dev$(tput rmul)..."
-    npm run build 
+    npm run build
 
     echo -en "\nVisit your site at https://$(ops project name).$OPS_DOMAIN/\n"
 }
