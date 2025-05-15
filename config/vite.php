69a9e7c01563a2d1734a70476dd4a0ddd5636341
Jeff Turcotte <jeff.turcotte@gmail.com>
2025-05-15 13:57:14 -0400

    Fixed issue with craft-vite config

--- config/vite.php
+++ config/vite.php
@@ -4,7 +4,7 @@ use craft\helpers\App;
 
 return [
     'useDevServer' => App::env('USE_VITE_DEV_SERVER') ?? false,
-    'manifestPath' => '@webroot/dist/manifest.json',
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
     'devServerPublic' => 'http://localhost:5173/',
     'serverPublic' => App::env('DEFAULT_SITE_URL') . '/dist/',
     'errorEntry' => '',
